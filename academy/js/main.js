4c4051df0e23621c63433b5553b735d9b49b44a4
Reiser9 <elmanamirhanov@mail.ru>
2022-05-29 20:35:37 +0500

    academy

---
+++ academy/js/main.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+
+    const close = () => {
+        $("body").removeClass("scroll");
+        $(".header").removeClass("fixed");
+        $(".modal__menu").removeClass("active");
+        $(".cross").removeClass("active");
+        $(".menu").addClass("active");
+    }
+
+    $(".menu").on("click", function(){
+        $("body").addClass("scroll");
+        $(".header").addClass("fixed");
+        $(".modal__menu").addClass("active");
+        $(this).removeClass("active");
+        $(".cross").addClass("active");
+    });
+
+    $(".cross").on("click", function(){
+        close();
+    });
+
+    $(window).resize(function(){
+		if($(window).width() > 998){
+			close();
+		}
+	});
+
+    $(".tenders__tab").on("click", function(){
+        $(".tenders__tab").removeClass("active");
+        $(this).addClass("active");
+        $(".tenders__content").removeClass("active");
+        $(".tenders__content[data-tab="+$(this).attr("data-tab")+"]").addClass("active");
+    });
+
+	$(".result__carousel").owlCarousel({
+        items: 1,
+        nav: true,
+        dots: false,
+        navText: ['<img src="img/prev.svg" alt="стрелка" class="arrow arrow__prev">',
+	    		  '<img src="img/next.svg" alt="стрелка" class="arrow arrow__next">'],
+        loop: true,
+        margin: 30
+    });
+
+    $(".teachers__carousel").owlCarousel({
+        items: 3,
+        center: true,
+        loop: true,
+        margin: 30,
+        responsive: {
+            0:{
+	            items: 1,
+                autoHeight: true
+	        },
+	        768:{
+	            items:2
+	        },
+	        1200:{
+	            items:3
+	        }
+        }
+    });
+
+});
