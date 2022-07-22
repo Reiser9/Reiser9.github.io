0efd474765fbea69a9c89451e83ea92c01904bdd
Reiser9 <63304397+Reiser9@users.noreply.github.com>
2022-07-22 16:39:45 +0500

    prom

--- prom/js/main.js
+++ prom/js/main.js
@@ -14,13 +14,33 @@ $(document).ready(function(){
     $(".tab").on("click", function(e){
         e.preventDefault();
 
-        $(".tab").removeClass("active");
-        $(".tab__content").removeClass("active");
+        $(this).siblings(".tab").removeClass("active");
+        $(this).parent(".info__tabs").siblings(".info__tab--content").children(".tab__content").removeClass("active");
 
         let tab = $(this).attr("data-tab");
 
         $(this).addClass("active");
         $(".tab__content[data-tab="+tab+"]").addClass("active");
     });
+
+    const close = () => {
+		$("body").removeClass("scroll");
+		$(".modal__menu").removeClass("active");
+	}
+
+	$(".menu").on("click", function(){
+		$("body").addClass("scroll");
+		$(".modal__menu").addClass("active");
+	});
+
+	$(".modal__menu--close").on("click", function(){
+		close();
+	});
+
+	$(window).resize(function(){
+		if($(window).width() > 768){
+			close();
+		}
+	});
 
 });
