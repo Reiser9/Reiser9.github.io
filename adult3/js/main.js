fe6f5b5b8d7606f9b1bc70d23013daf2bef601d2
Reiser9 <63304397+Reiser9@users.noreply.github.com>
2022-08-08 16:13:57 +0500

    adult3

--- adult3/js/main.js
+++ adult3/js/main.js
@@ -1,11 +1,10 @@
-$(document).ready(function(){
-
-	$(".up").on('click', function(e){
+$(document).ready(function () {
+    $(".up").on("click", function (e) {
         e.preventDefault();
-        $('body, html').animate({scrollTop: 0}, 400);    
+        $("body, html").animate({ scrollTop: 0 }, 400);
     });
 
-    $(".drop__mobile").on("click", function(e){
+    $(".drop__mobile").on("click", function (e) {
         e.preventDefault();
 
         $(this).toggleClass("active");
@@ -16,47 +15,74 @@ $(document).ready(function(){
         $(".menu").removeClass("active");
         $(".menu").attr("src", "img/1/menu.svg");
         $("body").removeClass("scroll");
-        $(".modal__menu").removeClass("active");
-    }
+        $(".modal__menu[data-modal='menu']").removeClass("active");
+    };
 
-    $(".menu").on("click", function(){
-        if($(this).hasClass("active")){
+    $(".menu").on("click", function () {
+        if ($(this).hasClass("active")) {
             close();
-        }
-        else{
+        } else {
+            $(".header__city--inner").removeClass("active");
+            $("body").removeClass("scroll3");
+            $(".modal__menu").removeClass("active");
+            
             $(this).attr("src", "img/1/cross.svg");
             $(this).addClass("active");
             $("body").addClass("scroll");
-            $(".modal__menu").addClass("active");
+            $(".modal__menu[data-modal='menu']").addClass("active");
         }
     });
 
-    $(".mn").on("click", function(){
-        if($(this).hasClass("active")){
+    $(".mn").on("click", function () {
+        if ($(this).hasClass("active")) {
             $(".mn").removeClass("active");
             $(".mn").attr("src", "img/1/menu-pk.svg");
             $("body").removeClass("scroll");
+            $(".modal__menu[data-modal='menu']").removeClass("active");
+        } else {
+            $(".header__city--inner").removeClass("active");
+            $("body").removeClass("scroll3");
             $(".modal__menu").removeClass("active");
-        }
-        else{
+
             $(this).attr("src", "img/1/cross.svg");
             $(this).addClass("active");
             $("body").addClass("scroll");
-            $(".modal__menu").addClass("active");
+            $(".modal__menu[data-modal='menu']").addClass("active");
         }
     });
 
-    $(".card__main--img").on("click", function(){
+    $(".open__modal").on("click", function (e) {
+        e.preventDefault();
+        $("body").removeClass("scroll");
+        $("body").removeClass("scroll2");
+        $(".modal__menu").removeClass("active");
+        $(".mn").attr("src", "img/1/menu-pk.svg");
+        $(".menu").attr("src", "img/1/menu.svg");
+        $(".mn").removeClass("active");
+        $(".menu").removeClass("active");
+
+        $(this).addClass("active");
+        $("body").addClass("scroll3");
+        $(".modal__menu[data-modal='citys']").addClass("active");
+    });
+
+    $(".modal__cross").on("click", function () {
+        $(".header__city--inner").removeClass("active");
+        $("body").removeClass("scroll3");
+        $(".modal__menu[data-modal='citys']").removeClass("active");
+    });
+
+    $(".card__main--img").on("click", function () {
         $("body").addClass("scroll2");
         $(".modal__carousel").addClass("active");
     });
 
-    $(".modal__carousel").on("click", function(){
+    $(".modal__carousel").on("click", function () {
         $("body").removeClass("scroll2");
         $(".modal__carousel").removeClass("active");
     });
 
-    $(".modal__carousel--inner").on("click", function(e){
+    $(".modal__carousel--inner").on("click", function (e) {
         e.stopPropagation();
     });
 
@@ -66,7 +92,7 @@ $(document).ready(function(){
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: false,
-        asNavFor: '.main__photo--inner'
+        asNavFor: ".main__photo--inner",
     });
 
     $(".main__photo--inner").slick({
@@ -75,16 +101,16 @@ $(document).ready(function(){
         infinite: true,
         slidesToShow: 6,
         slidesToScroll: 1,
-        asNavFor: '.card__main--img--inner',
+        asNavFor: ".card__main--img--inner",
         focusOnSelect: true,
         responsive: [
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 3
-                }
-            }
-        ]
+                    slidesToShow: 3,
+                },
+            },
+        ],
     });
 
     $(".modal__carousel--content").slick({
@@ -92,7 +118,6 @@ $(document).ready(function(){
         arrows: false,
         infinite: true,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
     });
-
 });
