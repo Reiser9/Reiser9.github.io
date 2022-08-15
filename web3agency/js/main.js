f5d8af436ea1419fc129a65faac629dd3d8cdf4e
Reiser9 <63304397+Reiser9@users.noreply.github.com>
2022-08-15 21:41:27 +0500

    web3agency

---
+++ web3agency/js/main.js
@@ -0,0 +1,38 @@
+$(window).on("load", function(){
+    $(".preloader").removeClass("active");
+
+	setInterval(function(){
+        if($(".spec1").hasClass("active")){
+            $(".spec1").removeClass("active");
+            $(".spec2").addClass("active");
+        }
+        else if($(".spec2").hasClass("active")){
+            $(".spec2").removeClass("active");
+            $(".spec3").addClass("active");
+        }
+        else{
+            $(".spec3").removeClass("active");
+            $(".spec1").addClass("active");
+        }
+    }, 3000);
+
+    $(".go").on("click", function(e){
+        e.preventDefault();
+
+        let point = $(this).attr("data-point");
+        let pointTop = $("#"+point).offset().top;
+        $('body,html').animate({scrollTop: pointTop}, 500);
+    });
+
+    $(".projects__tab").on("click", function(){
+        if(!$(this).hasClass("active")){
+            $(".projects__tab").removeClass("active");
+            $(this).addClass("active");
+
+            let tab = $(this).attr("data-tab");
+            $(".projects__content--tab").removeClass("active");
+            $(".projects__content--tab[data-tab="+tab+"]").addClass("active");
+        }
+    });
+
+});
