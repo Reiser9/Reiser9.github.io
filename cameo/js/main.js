18d9c36644e44a3009c4d780c93836116a79731d
Reiser9 <63304397+Reiser9@users.noreply.github.com>
2022-08-29 20:37:40 +0500

    cameo

--- cameo/js/main.js
+++ cameo/js/main.js
@@ -9,8 +9,75 @@ $(document).ready(function(){
         }
     }
 
+    const close = () => {
+        $("body").removeClass("scroll");
+        $(".modal__menu").removeClass("active");
+    }
+
+    scroll();
     $(window).scroll(function(){
         scroll();
+    });
+
+    $(".menu").on("click", function(){
+        $("body").addClass("scroll");
+        $(".modal__menu").addClass("active");
+    });
+
+    $(".cross").on("click", function(){
+        close();
+    });
+
+    $(window).resize(function(){
+		if($(window).width() > 998){
+			close();
+		}
+	});
+
+    $(".content__block--content").each(function () {
+        var slider = $(this);
+    
+        slider.slick({
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            centerMode: true,
+            speed: 200,
+            centerPadding: '0px',
+            prevArrow: slider.parent().find('.prev'),
+            nextArrow: slider.parent().find('.next'),
+            responsive: [
+                {
+                    breakpoint: 1421,
+                    settings: {
+                        slidesToShow: 5
+                    }
+                },
+                {
+                    breakpoint: 998,
+                    settings: {
+                        slidesToShow: 4
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 3
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 0,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                },
+            ]
+        });
     });
 
 });
