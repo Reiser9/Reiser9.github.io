6b1a9cd8ad454bfe29b86be449dc2f4937496154
Reiser9 <63304397+Reiser9@users.noreply.github.com>
2022-10-24 21:40:41 +0500

    adult6

---
+++ adult6/js/main.js
@@ -0,0 +1,102 @@
+$(document).ready(function(){
+
+    // Кнопка наверх
+    $(".up").on("click", function(e){
+        e.preventDefault();
+
+        $('body,html').animate({scrollTop: 0}, 400);
+    });
+
+    // Поиск на мобиле
+    $(".search__button").on("click", function(){
+        $(".search__mob").toggleClass("active");
+    });
+
+    // Меню
+    const close = () => {
+        $("body").removeClass("scroll");
+        $(".modal__menu").removeClass("active");
+    }
+
+    const open = () => {
+        $("body").addClass("scroll2");
+        $(".modal__city").addClass("active");
+    }
+
+    $(".menu__button").on("click", function(){
+        $("body").addClass("scroll");
+        $(".modal__menu").addClass("active");
+    });
+
+    $(".modal__cross").on("click", function(){
+        close();
+    });
+
+    $(window).resize(function(){
+        if($(window).width() > 998){
+            close();
+		}
+	});
+
+    $(".header__nav--link--inner .header__nav--link").on("click", function(e){
+        e.preventDefault();
+        if(!$(this).hasClass("active")){
+            $(".header__nav--drop").removeClass("active");
+            $(".header__nav--link").removeClass("active");
+        }
+        
+        $(this).siblings(".header__nav--drop").toggleClass("active");
+        $(this).toggleClass("active");
+    });
+
+    // Кнопка наверх
+    const ofset = () => {
+        if($(window).scrollTop() > 100){
+            $(".up__button").addClass("active");
+        }
+        else{
+            $(".up__button").removeClass("active");
+        }
+    }
+
+    $(window).scroll(function(){
+        ofset();
+    });
+
+    ofset();
+
+    // Показать форму отзыва
+    $(".review__leave").on("click", function(){
+        $(".card__reviews--show").removeClass("active");
+        $(".card__reviews--leave").addClass("active");
+    });
+
+    $(".review__send").on("click", function(){
+        $(".card__reviews--show").addClass("active");
+        $(".card__reviews--leave").removeClass("active");
+    });
+
+    // Слайдеры
+	$(".salon__item--img--slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true
+    });
+
+    $(".salon__slider--big").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.salon__slider--small'
+    });
+
+    $(".salon__slider--small").slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.salon__slider--big',
+        focusOnSelect: true
+    });
+
+});
