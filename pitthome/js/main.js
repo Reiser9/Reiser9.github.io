a9a3f9320adc190801f3c1e7f0242f8938b83789
Reiser9 <63304397+Reiser9@users.noreply.github.com>
2022-10-31 21:26:39 +0500

    pitthome

--- pitthome/js/main.js
+++ pitthome/js/main.js
@@ -11,4 +11,24 @@ $(document).ready(function(){
         },
     });
 
+    const close = () => {
+        $("body").removeClass("scroll");
+        $(".modal__menu").removeClass("active");
+    }
+
+    $(".menu").on("click", function(){
+        $("body").addClass("scroll");
+        $(".modal__menu").addClass("active");
+    });
+
+    $(".modal__cross").on("click", function(){
+        close();
+    });
+
+    $(window).resize(function(){
+        if($(window).width() > 998){
+            close();
+		}
+	});
+
 });
