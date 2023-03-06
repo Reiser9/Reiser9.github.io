dc952619587b6f4280d905d823f2971b29482adc
Reiser9 <63304397+Reiser9@users.noreply.github.com>
2023-03-06 17:24:56 +0500

    muslimcoin

---
+++ muslimcoin/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	$(".faq__item").on("click", function(){
+        if($(this).hasClass("active")){
+            $(this).removeClass("active");
+        }
+        else{
+            $(".faq__item").removeClass("active");
+            $(this).addClass("active");
+        }
+    });
+
+});
