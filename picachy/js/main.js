50e0d799b36015009b7c973f595a16eab7ff24ef
Reiser9 <63304397+Reiser9@users.noreply.github.com>
2023-05-19 21:20:29 +0500

    picachy

--- picachy/js/main.js
+++ picachy/js/main.js
@@ -10,13 +10,13 @@ $(document).ready(function(){
     const first = document.querySelector(".first__text");
     const second = document.querySelector(".second__text");
     const fird = document.querySelector(".fird__text");
+    const four = document.querySelector(".forth__text");
+    const five = document.querySelector(".five__text");
     
     const about = document.querySelector(".about");
     const scrollBottom = document.querySelector(".main__center--bottom");
 
     const main = document.querySelector(".main");
-    const poke = document.querySelector(".poke__content");
-    const tax = document.querySelector(".tax__mobile");
     
 
     scrollBottom.addEventListener("click", function(e){
@@ -28,44 +28,54 @@ $(document).ready(function(){
         });
     });
 
-    // function checkAnimation() {
-    //     if(window.scrollY > poke.clientHeight + tax.clientHeight + (main.clientHeight / 2)){
-    //         document.body.classList.add("overlay");
-    //     }
-    //     else{
-    //         document.body.classList.remove("overlay");
-    //     }
-    // }
-
-    // checkAnimation();
-
 	document.addEventListener("scroll", function(){
         const windowScroll = window.scrollY;
         const aboutScroll = about.offsetTop;
-
-        // Main animation
-        // checkAnimation();
         
         // Text about
         if(windowScroll - aboutScroll < 0 && windowScroll - aboutScroll > -100){
             first.classList.add("active");
             second.classList.remove("active");
             fird.classList.remove("active");
+            four.classList.remove("active");
+            five.classList.remove("active");
         }
         else if(windowScroll - aboutScroll >= 0 && windowScroll - aboutScroll <= 150){
             second.classList.add("active");
             first.classList.remove("active");
             fird.classList.remove("active");
+            four.classList.remove("active");
+            five.classList.remove("active");
         }
         else if(windowScroll - aboutScroll >= 150 && windowScroll - aboutScroll <= 250){
             fird.classList.add("active");
             second.classList.remove("active");
             first.classList.remove("active");
+            four.classList.remove("active");
+            five.classList.remove("active");
+        }
+        else if(windowScroll - aboutScroll >= 450 && windowScroll - aboutScroll <= 550){
+            four.classList.add("active");
+            second.classList.remove("active");
+            first.classList.remove("active");
+            fird.classList.remove("active");
+            five.classList.remove("active");
+        }
+        else if(windowScroll - aboutScroll >= 550 && windowScroll - aboutScroll <= 650){
+            five.classList.add("active");
+            second.classList.remove("active");
+            first.classList.remove("active");
+            fird.classList.remove("active");
+            four.classList.remove("active");
         }
         else{
             fird.classList.remove("active");
             second.classList.remove("active");
             first.classList.remove("active");
+            four.classList.remove("active");
+            five.classList.remove("active");
+            four.classList.remove("active");
+            five.classList.remove("active");
         }
     });
 
