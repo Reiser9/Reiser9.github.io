64a318d19c7d556a743d3e7fa791c978a136b413
Reiser9 <elmanamirhanov@mail.ru>
2024-09-26 12:17:28 +0500

    dar

--- dar/js/main.js
+++ dar/js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 
-	
+	$(".header__menu--link").on("click", function(e){
+        e.preventDefault();
+        const menu = $(this).attr("data-menu");
+        const targetPoint = $(".mobile__menu[data-menu="+menu+"]");
+
+        if(targetPoint.hasClass("active")){
+            $(".mobile__menu").removeClass("active");
+        }
+        else{
+            $(".mobile__menu").removeClass("active");
+            targetPoint.addClass("active");
+        }
+    });
 
 });
